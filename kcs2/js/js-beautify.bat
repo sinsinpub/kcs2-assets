247ad613e2f5ccab1871e0cb4005405103d473e9
sin_sin <sinsinpub@gmail.com>
2018-09-16 22:46:38 +0800

    Add event world 42

--- kcs2/js/js-beautify.bat
+++ kcs2/js/js-beautify.bat
@@ -3,5 +3,6 @@ setlocal
 set mainjs="D:\Download\main.js"
 if not "%1"=="" set mainjs="%1"
 echo Formatiing %mainjs%...
+rem npm install js-beautify
 node node_modules\js-beautify\js\bin\js-beautify.js --config .\jsb-config.json %mainjs% > main.formatted.js
 endlocal
