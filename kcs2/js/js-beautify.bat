c6485a79a22752adf1fd76215922887cf58dacf1
sin_sin <sinsinpub@gmail.com>
2019-12-13 12:51:41 +0800

    2019-12-12 4.5.1.3
    update curl script to follow 302 redirects

--- kcs2/js/js-beautify.bat
+++ kcs2/js/js-beautify.bat
@@ -5,7 +5,9 @@ if not "%1"=="" if not "%1"=="-s" set mainjs="%1"
 if "%1"=="-s" goto skipDownload
 if "%2"=="-s" goto skipDownload
 echo Downloading %mainjs%...
-curl -R -o %mainjs% "http://203.104.209.71/kcs2/js/main.js" -H "Referer: http://203.104.209.71/kcs2/index.php" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0 Safari/537.36"
+rem World 1: 203.104.209.71, World 3: 125.6.184.215
+set serverip=203.104.209.71
+curl -RL -o %mainjs% "http://%serverip%/kcs2/js/main.js" -H "Referer: http://%serverip%/kcs2/index.php" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0 Safari/537.36"
 :skipDownload
 echo Formatting %mainjs%...
 rem npm install js-beautify
