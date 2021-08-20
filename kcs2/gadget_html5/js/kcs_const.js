da27435626fb47d99b289d8dcd75b74f9212134c
sin_sin <sinsinpub@gmail.com>
2021-08-21 02:00:45 +0800

    2021-08-20 5.3.0.0

--- kcs2/gadget_html5/js/kcs_const.js
+++ kcs2/gadget_html5/js/kcs_const.js
@@ -54,12 +54,12 @@ ConnectionInfo.Interval_Min   = 10;
 var MaintenanceInfo = {};
 MaintenanceInfo.IsDoing       = 0;
 MaintenanceInfo.IsEmergency   = 0;
-MaintenanceInfo.StartDateTime = Date.parse("2021/08/04 00:00:00");
-MaintenanceInfo.EndDateTime   = Date.parse("2021/08/04 00:59:59");
+MaintenanceInfo.StartDateTime = Date.parse("2021/08/20 00:00:00");
+MaintenanceInfo.EndDateTime   = Date.parse("2021/08/20 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.2.4.0";
+VersionInfo.scriptVesion      = "5.3.0.0";
 
 // Entrance 情報
 var EntranceInfo = {};
