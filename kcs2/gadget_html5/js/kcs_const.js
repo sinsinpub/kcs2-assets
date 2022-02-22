d07225e465380075475b106afb2c76154251f315
sin_sin <sinsinpub@gmail.com>
2022-02-23 01:13:51 +0800

    2022-02-22 5.3.7.0

--- kcs2/gadget_html5/js/kcs_const.js
+++ kcs2/gadget_html5/js/kcs_const.js
@@ -54,12 +54,12 @@ ConnectionInfo.Interval_Min   = 10;
 var MaintenanceInfo = {};
 MaintenanceInfo.IsDoing       = 0;
 MaintenanceInfo.IsEmergency   = 0;
-MaintenanceInfo.StartDateTime = Date.parse("2022/01/21 00:00:00");
-MaintenanceInfo.EndDateTime   = Date.parse("2022/01/21 00:59:59");
+MaintenanceInfo.StartDateTime = Date.parse("2022/02/22 00:00:00");
+MaintenanceInfo.EndDateTime   = Date.parse("2022/02/22 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.3.6.6";
+VersionInfo.scriptVesion      = "5.3.7.0";
 
 // Entrance 情報
 var EntranceInfo = {};
