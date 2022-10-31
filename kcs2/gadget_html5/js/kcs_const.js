0be62ae059f3a7bee7b4dbd252e5c8449e6ee05a
sin_sin <sinsinpub@gmail.com>
2022-10-31 19:13:14 +0800

    2022-10-31 5.5.2.2

--- kcs2/gadget_html5/js/kcs_const.js
+++ kcs2/gadget_html5/js/kcs_const.js
@@ -59,7 +59,7 @@ MaintenanceInfo.EndDateTime   = Date.parse("2022/10/14 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.5.2.0";
+VersionInfo.scriptVesion      = "5.5.2.2";
 
 // Entrance 情報
 var EntranceInfo = {};
