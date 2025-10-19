a2a2c8768fc9ae2d1934aa71fdb85016e4f7b9fc
sin_sin <sinsinpub@gmail.com>
2025-10-19 18:56:08 +0800

    2025-10-17 6.1.7.0
    * No updated gadget login related files yet

--- kcs2/js/js-deobfuscate.bat
+++ kcs2/js/js-deobfuscate.bat
@@ -7,7 +7,7 @@ if "%2"=="-s" goto skipDownload
 echo Downloading %mainjs%...
 rem World 1: 203.104.209.71, World 3: 125.6.184.215
 set serverip=w01y.kancolle-server.com
-curl -RL -o %mainjs% "http://%serverip%/kcs2/js/main.js" -H "Referer: http://%serverip%/kcs2/index.php" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0 Safari/537.36"
+curl -RL -o %mainjs% "https://%serverip%/kcs2/js/main.js" -H "Referer: https://%serverip%/kcs2/index.php" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0 Safari/537.36"
 :skipDownload
 echo Deobfuscating %mainjs%...
 copy %mainjs% kancolle-main\dist\
--- kcs2/update-version.bat
+++ kcs2/update-version.bat
@@ -2,8 +2,8 @@
 setlocal
 set gadgetip=w00g.kancolle-server.com
 set serverip=w01y.kancolle-server.com
-set referer="Referer: http://%serverip%/kcs2/index.php"
+set referer="Referer: https://%serverip%/kcs2/index.php"
 set useragent="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0 Safari/537.36"
-curl -RL -o gadget_html5\js\kcs_const.js "http://%gadgetip%/gadget_html5/js/kcs_const.js" -H %referer% -H %useragent%
-curl -RL -o version.json "http://%serverip%/kcs2/version.json" -H %referer% -H %useragent%
+curl -RL -o gadget_html5\js\kcs_const.js "https://%gadgetip%/gadget_html5/js/kcs_const.js" -H %referer% -H %useragent%
+curl -RL -o version.json "https://%serverip%/kcs2/version.json" -H %referer% -H %useragent%
 endlocal
